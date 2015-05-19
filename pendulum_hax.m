1f9baebd723240ad565e55c6de8d835b1e75ea23
aremang <albin.remang@gmail.com>
2015-05-19 16:20:43 +0200

    Plots for att bevisa instabilitet osv

--- pendulum_hax.m
+++ pendulum_hax.m
@@ -4,14 +4,15 @@
 
 g = 9.82;
 l = 3.0;            %length of the swing
-A = 0.05*l;          %Amplitude of the oscillations
-gamma = 0.8;          %Friction coefficient
-omega = 80;      %Exitation frequency
+A = 0;          %Amplitude of the oscillations
+gamma = 0;          %Friction coefficient
+omega = 2*sqrt(g/l);      %Exitation frequency
 % Pendeln blir stabil för ca w >= 52
 
+
 tau = 0;            %Begynnelsevärde
-T = 1e3;             %End time
-x0 = [pi+0.1 0]';        %Randvillkor
+T = 20;             %End time
+x0 = [0.1 0]';        %Randvillkor
 
 f = @(t,x)[x(2);-gamma/omega*x(2)-(g-A*omega^2*cos(t))*sin(x(1))/(l*omega^2)];
 
@@ -26,24 +27,24 @@ plot(T,X(:,1));
 
 
 figure(2)
-% quiver(X(:,1),X(:,2),gradient(X(:,1)),gradient(X(:,2)))
-plot(X(:,1),X(:,2))
+quiver(X(:,1),X(:,2),gradient(X(:,1)),gradient(X(:,2)))
+% plot(X(:,1),X(:,2))
 title('Phase portrait')
 xlabel('Angle')
 ylabel('Angular velocity')
 % 
-% set(gca, ...
-% 	  'Box'         , 'off'     , ...
-% 	  'TickDir'     , 'out'     , ...
-% 	  'TickLength'  , [.02 .02] , ...
-% 	  'XMinorTick'  , 'on'      , ...
-% 	  'YMinorTick'  , 'on'      , ...
-% 	  'YGrid'       , 'on'      , ...
-% 	  'XColor'      , [.3 .3 .3], ...
-% 	  'YColor'      , [.3 .3 .3], ...
-% 	  'LineWidth'   , 1         );
-% 	set(gcf, 'PaperPositionMode', 'auto');
-% 	print -depsc2 fig/pp_up_w80_fric08.eps
+set(gca, ...
+	  'Box'         , 'off'     , ...
+	  'TickDir'     , 'out'     , ...
+	  'TickLength'  , [.02 .02] , ...
+	  'XMinorTick'  , 'on'      , ...
+	  'YMinorTick'  , 'on'      , ...
+	  'YGrid'       , 'on'      , ...
+	  'XColor'      , [.3 .3 .3], ...
+	  'YColor'      , [.3 .3 .3], ...
+	  'LineWidth'   , 1         );
+	set(gcf, 'PaperPositionMode', 'auto');
+	print -depsc2 fig/pp_down_2w_eps_0.eps
 
 
 
